e9ceea6f0c3b99cf46e0b1a2bf2c3ba34fdc1dc5
Antonio Artimon <antonio.artimon@student.tuiasi.ro>
2023-10-10 16:18:27 +0300

    Implementing the ViewModel

---
+++ app/src/main/java/com/tonio/weatherapp/presentation/WeatherState.kt
@@ -0,0 +1,10 @@
+package com.tonio.weatherapp.presentation
+
+import com.tonio.weatherapp.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+
+)
