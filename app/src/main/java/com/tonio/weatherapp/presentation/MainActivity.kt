8daff129467b79ccba0eea3c0763cf6c213df28c
Antonio Artimon <antonio.artimon@student.tuiasi.ro>
2023-10-10 19:01:52 +0300

    Implementing and testing the Compose UI

--- app/src/main/java/com/tonio/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/tonio/weatherapp/presentation/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.tonio.weatherapp.presentation
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,34 +18,46 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.tonio.weatherapp.presentation.ui.theme.DarkBlue
+import com.tonio.weatherapp.presentation.ui.theme.DeepBlue
 import com.tonio.weatherapp.presentation.ui.theme.WeatherAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: WeatherViewModel by viewModels()
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ){
+            viewModel.loadWeatherInfo()
+        }
+        permissionLauncher.launch(arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ))
         setContent {
             WeatherAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(DarkBlue)
                 ) {
-                    Greeting("Android")
+                    WeatherCard(state = viewModel.state, backgroundColor = DeepBlue)
                 }
+
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     WeatherAppTheme {
-        Greeting("Android")
     }
 }
