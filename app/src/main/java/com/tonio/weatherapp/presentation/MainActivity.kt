424d4ea892c2dc50a6dc5e18b915273742fd7f1f
Antonio Artimon <antonio.artimon@student.tuiasi.ro>
2023-10-11 14:07:24 +0300

    Finished UI

--- app/src/main/java/com/tonio/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/tonio/weatherapp/presentation/MainActivity.kt
@@ -10,14 +10,18 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tonio.weatherapp.presentation.ui.theme.DarkBlue
 import com.tonio.weatherapp.presentation.ui.theme.DeepBlue
 import com.tonio.weatherapp.presentation.ui.theme.WeatherAppTheme
@@ -42,13 +46,36 @@ class MainActivity : ComponentActivity() {
         ))
         setContent {
             WeatherAppTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(DarkBlue)
-                ) {
-                    WeatherCard(state = viewModel.state, backgroundColor = DeepBlue)
+                Box {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(DarkBlue)
+                    ) {
+
+                        WeatherCard(
+                            state = viewModel.state,
+                            backgroundColor = DeepBlue
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        WeatherForecast(state = viewModel.state)
+                    }
+                    if(viewModel.state.isLoading){
+                        CircularProgressIndicator(
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                    viewModel.state.error?.let { error ->
+                        Text(
+                            text = error,
+                            color = Color.Red,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(Alignment.Center)
+                            )
+
+                    }
                 }
+
 
             }
         }
